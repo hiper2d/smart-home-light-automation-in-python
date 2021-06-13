0a92c91f526a58f232864300b4fa6cd7ba1dcd4a
hiper2d <hiper2d@gmail.com>
2021-06-13 18:12:22 -0400

    Added backend to server side events

--- raspberrypi/website.py
+++ raspberrypi/website.py
@@ -1,11 +1,19 @@
 import threading
+from typing import List
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 
 from raspberrypi.mqtt_client import MqttClient
-
+from raspberrypi.util import MessageAnnouncer
 
 app = Flask(__name__)
+mqtt_client = MqttClient()
+announcer = MessageAnnouncer()
+
+
+def _on_message_announce(items: List[str]):
+    announcer.announce("ABC")
+    print(items)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,14 +27,24 @@ def index():
             print('unknown')
     elif request.method == 'GET':
         form = request.form
-        return render_template('index.html', form=form)
-
+        return render_template('index.html', form=form, devices=mqtt_client.get_devices_as_list())
     return render_template('index.html')
+
+
+@app.route('/listen', methods=['GET'])
+def listen():
+    def stream():
+        messages = announcer.listen()  # returns a queue.Queue
+        while True:
+            msg = messages.get()  # blocks until a new message arrives
+            yield msg
+    return Response(stream(), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
     mqtt_client = MqttClient()
     mqtt_client.start()
+    mqtt_client.on_message_finalize = _on_message_announce
     threading.Thread(target=mqtt_client.clean_dead_devices, daemon=True).start()
     while not mqtt_client.connected:
         pass
