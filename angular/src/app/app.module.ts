3ed904d7448e89da66a9c5b1786b253a45829040
hiper2d <hiper2d@gmail.com>
2021-06-20 14:21:25 -0400

    Added Angular Material

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButton} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButton
   ],
   providers: [],
   bootstrap: [AppComponent]
